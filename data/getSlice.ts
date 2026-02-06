d7d293b229ca36d7f2cd2557b3cb0615e87c8fce
Jopa10 <99352783+Jopa10@users.noreply.github.com>
2026-02-06 18:24:27 +0000

    Create getSlice.ts

---
+++ data/getSlice.ts
@@ -0,0 +1,15 @@
+export const slices = {
+  "leeds/admin-officer": {
+    city: "Leeds",
+    role: "Admin Officer",
+    jobs: [
+      {
+        title: "Admin Officer",
+        company: "NHS",
+        location: "Leeds",
+        salary: "£22,000–£24,000",
+        applyUrl: "#",
+      },
+    ],
+  },
+}
