db57bb9ffffb16d747c90d24e654f635a8838bb6
Jopa10 <99352783+Jopa10@users.noreply.github.com>
2026-04-09 21:10:03 +0100

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,11 +3,15 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.ontapjobsearch.com',
+      url: 'https://www.ontapjobsearch.com/',
       lastModified: new Date(),
     },
     {
-      url: 'https://www.ontapjobsearch.com/leeds/nhs-admin-jobs',
+      url: 'https://www.ontapjobsearch.com/west-yorkshire/support-worker',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.ontapjobsearch.com/south-yorkshire/support-worker',
       lastModified: new Date(),
     }
   ]
