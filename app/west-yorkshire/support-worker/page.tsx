b6b4cfbb9d5abd4520090b7d256a3ff48272c6bd
Jopa10 <99352783+Jopa10@users.noreply.github.com>
2026-05-08 12:15:52 +0100

    Update page.tsx

--- app/west-yorkshire/support-worker/page.tsx
+++ app/west-yorkshire/support-worker/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
       jsonPath={["app", "west-yorkshire", "support-worker.json"]}
       region="West Yorkshire"
       title="West Yorkshire Support Worker Roles"
-      latestUpdate="Thu 7th May, AM"
+      latestUpdate="Fri 8th May, AM"
       anchorTown="Leeds"
     />
   );
