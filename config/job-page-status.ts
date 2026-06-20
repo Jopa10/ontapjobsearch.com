9582b2c68d6351ac48ae9a934c24f58b79413321
Jopa10 <99352783+Jopa10@users.noreply.github.com>
2026-06-20 11:09:25 +0100

    Update job page status and dates

--- config/job-page-status.ts
+++ config/job-page-status.ts
@@ -1,13 +1,13 @@
 export const jobPageStatus = {
   defaultStatus: "Updated",
-  defaultDate: "Fri 19th June, AM",
+  defaultDate: "Sat 20th June, AM",
 
   routeStatus: {
-    "west-yorkshire/support-worker": "Checked",
-    "south-yorkshire/support-worker": "Checked",
-    "north-east/support-worker": "Checked",
-    "west-yorkshire/service-administrator-jobs": "Checked",
-    "south-yorkshire/service-administrator-jobs": "",
+    "west-yorkshire/support-worker": "Updated",
+    "south-yorkshire/support-worker": "Updated",
+    "north-east/support-worker": "Updated",
+    "west-yorkshire/service-administrator-jobs": "Updated",
+    "south-yorkshire/service-administrator-jobs": "Updated",
   },
 };
 
