970d4d471a5ae2db4acd077aa4d2520961ab581a
Jopa10 <99352783+Jopa10@users.noreply.github.com>
2026-07-27 09:59:27 +0100

    Update defaultDate in jobPageStatus

--- config/job-page-status.ts
+++ config/job-page-status.ts
@@ -1,6 +1,6 @@
 export const jobPageStatus = {
   defaultStatus: "Updated",
-  defaultDate: "Sun 26th July, AM",
+  defaultDate: "Mon 27th July, AM",
 
   routeStatus: {
     "west-yorkshire/support-worker": "Updated",
