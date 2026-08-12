fa6c755c3d2cbe9d7d7f5b97779460c165681668
Jopa10 <99352783+Jopa10@users.noreply.github.com>
2026-08-12 10:57:45 +0100

    Allow client-side session loading in public layout

--- components/SessionProvider.tsx
+++ components/SessionProvider.tsx
@@ -2,14 +2,18 @@
 'use client';
 
 import { SessionProvider as NextAuthSessionProvider } from 'next-auth/react';
-import { Session } from 'next-auth';
+import type { Session } from 'next-auth';
 
 export default function SessionProvider({
     children,
     session,
 }: {
     children: React.ReactNode;
-    session: Session | null;
+    session?: Session | null;
 }) {
+    if (session === undefined) {
+        return <NextAuthSessionProvider>{children}</NextAuthSessionProvider>;
+    }
+
     return <NextAuthSessionProvider session={session}>{children}</NextAuthSessionProvider>;
 }
